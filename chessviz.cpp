6fb59d10ae0e3f16b64a81ca350ba88eb49953ea
Kirill Mironenko <kchipson@yandex.ru>
2019-02-18 18:42:05 +0700

    Change output function

--- chessviz.cpp
+++ chessviz.cpp
@@ -4,11 +4,17 @@
 void out_board_consol(char board[8][8]){ // Функция отрисовки доски
 	int i,j;
 	for (i=7; i>-1;i--){
+		cout<<i+1<<" ";
 		for (j=0; j<8;j++){
-			cout<<board[i][j]<<"  ";
+			cout<<board[i][j]<<" ";
 		}
 		cout<<endl;
 	}
+	cout<<"  ";
+	for (i=97;i<105;i++)
+		cout<<(char)i<<" ";
+	cout<<endl;
+
 }
 
 int main(){ 
