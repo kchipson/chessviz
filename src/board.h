f3581bac681cb71820ef9097676f5efc91b24fe4
Kirill Mironenko <kchipson@yandex.ru>
2019-04-16 23:39:24 +0700

    Adding checks the movement of kNights

--- src/board.h
+++ src/board.h
@@ -1,3 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include "board_print_html.h"
+#include "board_print_plain.h"
+#include "board_read.h"
+#include "checks.h"
+#include <regex>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+void print(string out);
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "board_print_html.h"
-#include "board_print_plain.h"
 #include "board_read.h"
+#include "board.h"
 #include "checks.h"
 #include <cstdlib>
 #include <fstream>
@@ -8,7 +8,6 @@
 #include <stdio.h>
 using namespace std;
 
-extern char boardc[8][8];
 void checkFlag(string str){
     if ((str == string("--browser")) || (str == string("-b")))
         print_html_create();
@@ -21,10 +20,7 @@ void checkFlag(string str){
     }
 }
 int main(int argc, char* argv[])
-{ 
-    string buffer;
-    string moveWhite, moveBlack;
-   
+{    
     switch (argc)
     {
     case 1:
@@ -51,63 +47,6 @@ int main(int argc, char* argv[])
     if (checksMoves())
         return 1;
 
-    ifstream file_in("./bin/temp/notation.txt"); // открытие файла для чтения
-    while (!file_in.eof()) {
-        int i = 0;
-        moveWhite= moveBlack="";
-        getline(file_in, buffer); // считывание строки из файла
-        if (buffer != "") {
-            
-            while (buffer[i] != ' ')
-                i++;
-            i++;
-            while (buffer[i] != ' ') {
-                moveWhite += buffer[i];
-                i++;
-            }
-            i++;
-            while (i != (int)buffer.length()) {
-                moveBlack += buffer[i];
-                i++;
-            };
-
-            if ((string(argv[1]) == string("--browser"))
-                || (string(argv[1]) == string("-b"))) {
-                boardc[(moveWhite[1] - '0' - 1)][(moveWhite[0] - 'a')] = ' ';
-                boardc[(moveWhite[4] - '0' - 1)][(moveWhite[3] - 'a')] = 'P';
-                print_html(
-                        boardc,
-                        (buffer
-                         + "<span style='color: #ff6600'> <ход белых> "
-                           "</span>"));
-                boardc[(moveBlack[1] - '0' - 1)][(moveBlack[0] - 'a')] = ' ';
-                boardc[(moveBlack[4] - '0' - 1)][(moveBlack[3] - 'a')] = 'p';
-                print_html(
-                        boardc,
-                        (buffer
-                         + "<span style='color: #000092'> <ход черных> "
-                           "</span>"));
-
-            } else {
-                boardc[(moveWhite[1] - '0' - 1)][(moveWhite[0] - 'a')] = ' ';
-                boardc[(moveWhite[4] - '0' - 1)][(moveWhite[3] - 'a')] = 'P';
-                print_consol(
-                        boardc, (buffer + "\x1b[1;35m <ход белых> \x1b[1;31m"));
-                boardc[(moveBlack[1] - '0' - 1)][(moveBlack[0] - 'a')] = ' ';
-                boardc[(moveBlack[4] - '0' - 1)][(moveBlack[3] - 'a')] = 'p';
-                print_consol(
-                        boardc,
-                        (buffer + "\x1b[1;35m <ход черных> \x1b[1;31m"));
-            }
-        }
-        if (buffer[buffer.length() - 1] == '#')
-            break;
-    }
-    if ((string(argv[1]) == string("--browser"))
-        || (string(argv[1]) == string("-b"))) {
-        print_html();
-        system("sensible-browser bin/temp/chessviz.html");
-    }
-
+    print(argv[1]);
     return 0;
 }
